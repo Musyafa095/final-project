8b6f5d0fe35b9f5669aac1c4bec8568a7a356b27
Musyafa095 <musyafa095@gmail.com>
2025-01-14 12:29:35 +0700

    CRUD genre

---
+++ client/be-Review-Film/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<header>
+<Navbar/>
+</header>
+<main>
+    <Genre/>
+    <Movie/>
+</main>
+</template>
+
+<script setup>
+import {onMounted, ref} from 'vue'
+import { defineAsyncComponent } from 'vue';
+
+const Navbar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+const Genre = defineAsyncComponent (() => import('@/components/GenreMovie.vue'));
+const Movie = defineAsyncComponent(() => import ('@/components/MovieView.vue'))
+
+
+
+
+
+
+
+</script>
---
+++ client/be-Review-Film/src/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+import 'font-awesome/css/font-awesome.css';
+
+
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
